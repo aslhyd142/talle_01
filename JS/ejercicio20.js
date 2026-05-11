870f4cd2dc41aef240e3bd8c134b2f13556c780b
aslhyd123 <aslhyd142@gmail.com>
2026-05-11 17:39:43 -0500

    commit

---
+++ JS/ejercicio20.js
@@ -0,0 +1,17 @@
+usuario="aslhy"
+clave=4562
+if(usuario=="aslhy" )
+    console.log("usuario correcto")
+        else  {console.log ("usuario no encontrado / sin registrar" )
+            if (clave==123){
+
+console.log ("contraseña correcta")
+}
+
+else  {  console.log("clave incorrecta")}}
+
+
+
+if ("usuario no encontrado / sin registrar")
+{console.log ("clave null")
+}
